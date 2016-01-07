f79c768231ccefaba45b36d535369c422deeba83
Peter Clotworthy <clotters@gmail.com>
2016-01-07 06:24:16 +0000

    [FIX] Clean copies (that don't have vendor folders) fail because propel doesn't exist yet

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+php composer.phar update -o
+
 php vendor/propel/propel/bin/propel.php config:convert
 php vendor/propel/propel/bin/propel.php model:build
 php vendor/propel/propel/bin/propel.php diff
 php vendor/propel/propel/bin/propel.php migrate
 
-php composer.phar update -o
 echo "Finished!"
